1096c852f8ffa8cd98629959917e10be082d248f
Chris <cjr47@cornell.edu>
2021-12-02 18:59:29 -0500

    play game function

--- mystery_game.py
+++ mystery_game.py
@@ -6,20 +6,29 @@ print(board)
 
 
 def guess_letter(word):
-    guess = input('Guess a letter ')
-    
+    guess = input('Guess a letter ') 
+   
     if guess not in word:
-        print('Not in word :( Guess again!!')
+            print('Not in word :( Guess again!!')      
     else: 
-        # line 15 assigns indexes to each character
-        # range determines the collection of numbers in order in word_to_guess
-        # index is the position for each letter
-        for index in range(len(word_to_guess)):
-            if guess == word_to_guess[index]:
-                board[index] = guess
-                print(board)
-            #not needed: print(list(new_board))
-            #for letter in word_to_guess:
+            # line 15 assigns indexes to each character
+            # range determines the collection of numbers in order in word_to_guess
+            # index is the position for each letter
+            for index in range(len(word_to_guess)):
+                if guess == word_to_guess[index]:
+                    board[index] = guess
+                    print(board)    
+            #print('Congrats!! You guessed correctly')
+            return 
+        #guess_letter(word)
+                #not needed: print(list(new_board))
+                #for letter in word_to_guess:
 
-guess_letter(word_to_guess)    
+def play_game(word):
+    while word != board:
+        guess_letter(word)
+
+play_game(word_to_guess)
+
+#guess_letter(word_to_guess)    
 # maybe not use: for letter in enumerate(word_to_guess):
