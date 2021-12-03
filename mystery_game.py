44322fc43bf8b29e394df1b830866d3d0f25fe64
Chris <cjr47@cornell.edu>
2021-12-02 19:25:10 -0500

    added counter and number of tries

--- mystery_game.py
+++ mystery_game.py
@@ -5,28 +5,34 @@ print(board)
 #return board with letter guess substitute
 
 
-def guess_letter(word):
+def guess_letter(word, counter):
     guess = input('Guess a letter ') 
-   
     if guess not in word:
-            print('Not in word :( Guess again!!')      
+        counter += 1
+        print(counter)
+        print('Not in word :( Guess again!!')      
     else: 
             # line 15 assigns indexes to each character
             # range determines the collection of numbers in order in word_to_guess
             # index is the position for each letter
-            for index in range(len(word_to_guess)):
-                if guess == word_to_guess[index]:
+        for index in range(len(word_to_guess)):
+            if guess == word_to_guess[index]:
                     board[index] = guess
                     print(board)    
             #print('Congrats!! You guessed correctly')
-            return 
+    return counter
         #guess_letter(word)
                 #not needed: print(list(new_board))
                 #for letter in word_to_guess:
 
 def play_game(word):
-    while word != board:
-        guess_letter(word)
+    counter = 0
+    while word != board and counter < 8:
+        counter = guess_letter(word, counter)
+    if word != board:
+        print('Bummer! You ran out of tries!' + 'You Lost')
+    else:
+        print('Congrats!! You won!')
 
 play_game(word_to_guess)
 
